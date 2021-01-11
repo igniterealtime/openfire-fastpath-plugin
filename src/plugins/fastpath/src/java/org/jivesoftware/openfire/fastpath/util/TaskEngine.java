igniterealtime/openfire-monitoring-plugin

OF-993: Replace thread factory boilerplate code.

src/plugins/monitoring/src/java/org/jivesoftware/openfire/reporting/util/TaskEngine.java
@@ -19,6 +19,7 @@
 
 package org.jivesoftware.openfire.reporting.util;
 
+import org.jivesoftware.util.NamedThreadFactory;
 import org.picocontainer.Disposable;
 
 import java.util.*;
@@ -57,22 +58,8 @@ public class TaskEngine implements Disposable {
      */
     private TaskEngine() {
         timer = new Timer("timer-monitoring", true);
-        executor = Executors.newCachedThreadPool(new ThreadFactory() {
-
-            final AtomicInteger threadNumber = new AtomicInteger(1);
-
-            public Thread newThread(Runnable runnable) {
-                // Use our own naming scheme for the threads.
-                Thread thread = new Thread(Thread.currentThread().getThreadGroup(), runnable,
-                                      "pool-monitoring" + threadNumber.getAndIncrement(), 0);
-                // Make workers daemon threads.
-                thread.setDaemon(true);
-                if (thread.getPriority() != Thread.NORM_PRIORITY) {
-                    thread.setPriority(Thread.NORM_PRIORITY);
-                }
-                return thread;
-            }
-        });
+        final ThreadFactory threadFactory = new NamedThreadFactory( "pool-monitoring", true, Thread.NORM_PRIORITY, Thread.currentThread().getThreadGroup(), 0L );
+        executor = Executors.newCachedThreadPool( threadFactory );
     }
 
     /**

igniterealtime/openfire-fastpath-plugin

src/plugins/fastpath/src/java/org/jivesoftware/openfire/fastpath/util/TaskEngine.java
@@ -19,6 +19,8 @@
 
 package org.jivesoftware.openfire.fastpath.util;
 
+import org.jivesoftware.util.NamedThreadFactory;
+
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -55,22 +57,8 @@ public class TaskEngine {
      */
     private TaskEngine() {
         timer = new Timer("timer-fastpath", true);
-        executor = Executors.newCachedThreadPool(new ThreadFactory() {
-
-            final AtomicInteger threadNumber = new AtomicInteger(1);
-
-            public Thread newThread(Runnable runnable) {
-                // Use our own naming scheme for the threads.
-                Thread thread = new Thread(Thread.currentThread().getThreadGroup(), runnable,
-                                      "pool-fastpath" + threadNumber.getAndIncrement(), 0);
-                // Make workers daemon threads.
-                thread.setDaemon(true);
-                if (thread.getPriority() != Thread.NORM_PRIORITY) {
-                    thread.setPriority(Thread.NORM_PRIORITY);
-                }
-                return thread;
-            }
-        });
+        final ThreadFactory threadFactory = new NamedThreadFactory( "pool-fastpath", true, Thread.NORM_PRIORITY, Thread.currentThread().getThreadGroup(), 0L );
+        executor = Executors.newCachedThreadPool( threadFactory );
     }
 
     /**